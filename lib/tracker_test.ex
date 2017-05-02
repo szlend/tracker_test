36135848704c1f2c7ff521d61ab42fb0fe881bd7
Simon Žlender <simon.zlender@gmail.com>
2017-05-02 15:42:12 +0200

    Generate project

---
+++ lib/tracker_test.ex
@@ -0,0 +1,18 @@
+defmodule TrackerTest do
+  @moduledoc """
+  Documentation for TrackerTest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrackerTest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tracker_test_test.exs
@@ -0,0 +1,8 @@
+defmodule TrackerTestTest do
+  use ExUnit.Case
+  doctest TrackerTest
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
